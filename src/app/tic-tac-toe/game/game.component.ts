311867d6e57455a07161f73585efad572fd2fd91
IzabelaBorkowska <i.borkowska@pregiotek.com>
2021-07-12 11:09:30 +0200

    add click Events + 2 different turns

--- src/app/tic-tac-toe/game/game.component.ts
+++ src/app/tic-tac-toe/game/game.component.ts
@@ -3,16 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+
+  // @D why undefined needed, should go without initialization  in Angular
+  private currentTurn : number | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   //  TODO initialize/clean the game
+    this.currentTurn = 1;
   }
 
+  // @D possible in Observable ? which form is better
+  async clickCurrentCell(gameCell : any ): Promise<void>{
+    let position = gameCell.currentTarget.getAttribute('position');
+    console.log(position);
+
+    if(gameCell.currentTarget.innerHTML == ""){
+      this.currentTurn === 1 ? gameCell.currentTarget.innerHTML= "<p>x</p>": gameCell.currentTarget.innerHTML= "<p>o</p>";
+    }
+    this.newTurn();
+  }
+
+  private newTurn() : void{
+    this.currentTurn === 1 ? this.currentTurn = 2 : this.currentTurn = 1;
+    console.log("currentTurn" + this.currentTurn);
+  }
 
 
 }
--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tic-tac-toe',
   templateUrl: './tic-tac-toe.component.html',
-  styleUrls: ['./tic-tac-toe.component.css']
+  styleUrls: ['./tic-tac-toe.component.scss']
 })
 export class TicTacToeComponent implements OnInit {
 
