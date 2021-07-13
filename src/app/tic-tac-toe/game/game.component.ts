869ac040fd8c4ad667c05c07c0a6c60b1d024e38
IzabelaBorkowska <borkowska.pro@gmail.com>
2021-07-13 15:34:19 +0200

    prepare arrays to get result of game

--- src/app/tic-tac-toe/game/game.component.ts
+++ src/app/tic-tac-toe/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-game',
@@ -8,37 +9,74 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
 
   // @D why undefined needed, should go without initialization  in Angular
-  private currentTurn : number | undefined;
+  private currentTurn: number | undefined;
   //TODO change to false
   public gameStarted = true;
+  public gameWon = false;
+  private positionsPlayer1: number[] = [];
+  private positionsPlayer2: number[] = [];
 
-  constructor() { }
 
-  ngOnInit(): void {
-  //  TODO initialize/clean the game
-
+  constructor() {
   }
 
-  // @D possible in Observable ? which form is better
-  async clickCurrentCell(gameCell : any ): Promise<void>{
-    let position = gameCell.currentTarget.getAttribute('position');
-    console.log(position);
+  ngOnInit(): void {
+    //  TODO initialize/clean the game
+    this.currentTurn = 1;
+  }
 
-    if(gameCell.currentTarget.innerHTML == ""){
-      this.currentTurn === 1 ? gameCell.currentTarget.innerHTML= "<p>x</p>": gameCell.currentTarget.innerHTML= "<p>o</p>";
+
+  // @D possible in Observable ? which form is better?
+  async clickCurrentCell(gameCell: any): Promise<void> {
+    let position = gameCell.currentTarget.getAttribute('position');
+    // console.log( "position" + position.type);
+    // console.log(position);
+
+    if (gameCell.currentTarget.innerHTML == "") {
+      this.currentTurn === 1 ? this.positionsPlayer1.push(position) : this.positionsPlayer2.push(position);
+      this.currentTurn === 1 ? gameCell.currentTarget.innerHTML = "<p>x</p>" : gameCell.currentTarget.innerHTML = "<p>o</p>";
+      console.log(this.positionsPlayer1);
+      console.log(this.positionsPlayer2);
+
+      //  @D how to get number in the list - currently string
+      // this.positionsPlayer1.push(0)
+      // this.positionsPlayer1.push(1)
+      // let check = this.positionsPlayer1.includes(0,1);
+      // console.log( "position" + typeof this.positionsPlayer1[0]);
+      // console.log(position);
+
+
+      // this.checkIfWinner(this.positionsPlayer1);
+      this.checkIfGameOver();
       this.newTurn();
     }
 
   }
 
-  private newTurn() : void{
+  private newTurn(): void {
     this.currentTurn === 1 ? this.currentTurn = 2 : this.currentTurn = 1;
     console.log("currentTurn" + this.currentTurn);
   }
 
-  private startGame(): void{
+  public startGame(): void {
     this.currentTurn = 1;
     this.gameStarted = true;
+    window.location.reload();
+  }
+
+  private checkIfGameOver(): void {
+    if (this.positionsPlayer1.length === 5 && this.positionsPlayer2.length === 4 && !this.gameWon) {
+      alert("game over");
+
+    }
+  }
+
+  private checkIfWinner(arrayOfPlayerFields: number[]): void {
+
+    if (arrayOfPlayerFields) {
+      console.log(arrayOfPlayerFields.includes(0));
+      alert("you won");
+    }
   }
 
 
