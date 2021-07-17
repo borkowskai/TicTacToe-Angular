1c7d29004c68c435a2a1a175f356d5d281342b3c
IzabelaBorkowska <borkowska.pro@gmail.com>
2021-07-17 07:29:33 +0200

    correct problem with refresh window

--- src/app/tic-tac-toe/game/game.component.ts
+++ src/app/tic-tac-toe/game/game.component.ts
@@ -9,7 +9,7 @@ import * as _ from 'lodash';
 export class GameComponent implements OnInit {
 
   // @D why undefined needed, should go without initialization  in Angular
-  private currentTurn: number | undefined;
+  private currentTurn: number = 1;
   public gameStarted = false;
   public gameWon = false;
   private positionsPlayer1: number[] = [];
@@ -25,9 +25,9 @@ export class GameComponent implements OnInit {
 
   clickCurrentCell(gameCell: any, position : number): void {
     if (gameCell.currentTarget.innerHTML == "") {
+      console.log("currentTurn" + this.currentTurn);
       this.currentTurn === 1 ? this.positionsPlayer1.push(position) : this.positionsPlayer2.push(position);
       this.currentTurn === 1 ? gameCell.currentTarget.innerHTML = "<p>x</p>" : gameCell.currentTarget.innerHTML = "<p>o</p>";
-
       this.checkIfWinner(this.currentTurn === 1 ? this.positionsPlayer1 : this.positionsPlayer2);
       this.checkIfGameOver();
       this.newTurn();
@@ -37,18 +37,17 @@ export class GameComponent implements OnInit {
 
   private newTurn(): void {
     this.currentTurn === 1 ? this.currentTurn = 2 : this.currentTurn = 1;
-    console.log("currentTurn" + this.currentTurn);
   }
 
   public startGame(): void {
-    this.currentTurn = 1;
     this.gameStarted = true;
   }
 
   private checkIfGameOver(): void {
     if (this.positionsPlayer1.length === 5 && this.positionsPlayer2.length === 4 && !this.gameWon) {
       alert("game over");
-      this.startGame();
+
+      window.location.reload();
     }
   }
 
@@ -66,7 +65,7 @@ export class GameComponent implements OnInit {
         this.isInArray(array, 2,4,6 ) ){
         alert("you won");
         this.gameWon = true;
-        this.startGame();
+        window.location.reload();
       }
     }
   }
